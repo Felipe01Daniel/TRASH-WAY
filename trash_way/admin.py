80fe133e3d2a68a8200cd534ee22823c81051c07
mayaraassef <mayaraassef@gmail.com>
2019-12-06 22:59:23 -0300

    configurando administracao do django

--- trash_way/admin.py
+++ trash_way/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Lixo
 # Register your models here.
+
+@admin.register(Lixo)
+class LixoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cidade']
--- trash_way/models.py
+++ trash_way/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Lixo(models.Model):
+    cidade = models.CharField(max_length=50)
+    descricao = models.TextField()
+    estado_de_conservacao = models.TextField()
+    telefone = models.CharField(max_length=11)
+    email = models.EmailField()
+    photo = models.ImageField()
+    begin_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        db_table = 'lixo'
+
