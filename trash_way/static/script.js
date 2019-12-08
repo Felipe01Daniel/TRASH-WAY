df36ef06ee597fc5e9bfecdf5552bac80dadf73c
Danielgit-web <daniel.per.sns@gmail.com>
2019-12-08 00:39:22 -0300

    Código do mapa atualizado e novos marcadores adicionados

--- trash_way/static/script.js
+++ trash_way/static/script.js
@@ -1,7 +1,3 @@
-function initialize() {
-    initMap();
- }
-
 function initMap() {
         //opções mapa
     let options = {
@@ -272,25 +268,175 @@ function initMap() {
     google.maps.Map(document.getElementById("map"), options)
 
         //Array de marcadores
-    let markers = [
-        {       //Santa Cruz
-            coords: {lat:-23.5996933, lng: -46.6231753},
-            iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
-            content: '<h2 style="text-align: center;">Ecoponto Santa Cruz</h2> <img class="icone" src="images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Santa Cruz, 1452 - Vila Mariana</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
-        },
+        let markers = [
+            {       //Santa Cruz
+                coords: {lat:-23.5996933, lng: -46.6231753},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Santa Cruz</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Santa Cruz, 1452 - Vila Mariana</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Vila Mariana
+                coords: {lat:-23.5929202, lng: -46.6349351},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Vila Mariana</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Afonso Celso, 147 - Vila Mariana</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Vicente Rao
+                coords: {lat:-23.628375, lng: -46.685577},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Vicente Rao</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Av. Professor Vicente Rao, 308 - Santo Amaro</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Brás
+                coords: {lat:-23.554468, lng: -46.610806},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Brás</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Palmorino Mônaco - Mooca,</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Viaduto Antártica
+                coords: {lat:-23.524417, lng: -46.670641},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Viaduto Antártica</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Rua Robert Bosch, 242 - Parque Industrial Tomas Edson</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Alvarenga
+                coords: {lat:-23.695453, lng: -46.651554},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Alvarenga</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Estr. do Alvarenga, 2475 - Jardim Itapura</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Vila Madalena
+                coords: {lat:-23.558397, lng: -46.687488},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Vila Madalena</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Girassol, 15 - Vila Madalena</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Vila Mariana
+                coords: {lat:-23.593116, lng: -46.635120},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Vila Mariana</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Afonso Celso, 147 - Vila Mariana</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Vila Berrini
+                coords: {lat:-23.597401, lng: -46.691452},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Berrini</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Praça do Cancioneiro, 15 - Cidade Monções</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+            
+            {	//Ecoponto Tereza Cristina
+                coords: {lat:-23.569094, lng: -46.608511},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Berrini</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Av. Teresa Cristina, 10 - Ipiranga</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Bresser
+                coords: {lat:-23.543457, lng: -46.606410},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Berrini</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Praça Giuseppe Cesari, 54 - Brás</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+            
+            {	//Ecoponto Tatuapé
+                coords: {lat:-23.529909, lng: -46.583025},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Tatuapé</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Av. Salim Farah Maluf, 179 - Tatuapé</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Vila das Mercês
+                coords: {lat:-23.623759, lng: -46.605431},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Vila das Mercês</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Italva, 86 - Vila Moraes</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Imigrantes
+                coords: {lat:-23.632373, lng: -46.630577},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Imigrantes</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Rua Opixé - Vila Guarani</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Saioá
+                coords: {lat:-23.594423, lng: -46.622648},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Saioá</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Rua Professor Oswaldo Lacerda Gomes Cardim - Chácara Klabin</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+    
+            {	//Ecoponto Mirandópolis
+                coords: {lat:-23.611692, lng: -46.644651},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Mirandópolis</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Av. Senador Casemiro da Rocha, 1220</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
 
-        {	//Vila Mariana
-            coords: {lat:-23.5929202, lng: -46.6349351},
-            iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
-            content: '<h2 style="text-align: center;">Ecoponto Vila Mariana</h2> <img class="icone" src="images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Afonso Celso, 147 - Vila Mariana</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
-        },
+            {	//Ecoponto Vila Nova
+                coords: {lat:-23.472241, lng: -46.669897},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Vila Nova</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Rua Felix Alves Pereira, 113 - Vila Nova Cachoeirinha</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
 
-        {	//Vicente Rao
-            coords: {lat:-23.628375, lng: -46.685577},
-            iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
-            content: '<h2 style="text-align: center;">Ecoponto Vicente Rao</h2> <img class="icone" src="images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Av. Professor Vicente Rao, 308 - Santo Amaro</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
-        },
-    ]
+            {	//Ecoponto Cônego José Salomon
+                coords: {lat:-23.492242, lng: -46.721936},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Cônego José Salomon</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Av. Cônego José Salomon, 861 - Jardim Felicidade</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Cupecê
+                coords: {lat:-23.665439, lng: -46.654224},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Cupecê</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Anália Maria de Jesus, 131 - Jardim Itacolomi</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+            
+            {	//Ecoponto Liberdade
+                coords: {lat:-23.556584, lng: -46.637028},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Cupecê</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Rua Jaceguai, 67 - Liberdade</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Tiquatira
+                coords: {lat:-23.513541, lng: -46.543706},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Cupecê</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Av. Governador Carvalho Pinto - Jardim Mariana</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Tucuruvi
+                coords: {lat:-23.467260, lng: -46.609947},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Cupecê</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Eduardo Vicente Nasser, 519 - Jardim Leonor Mendes de Barros</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Jardim Maria do Carmo
+                coords: {lat:-23.595602, lng: -46.746835},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Jardim Maria do Carmo</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Caminho do Engenho, 800 - Ferreira</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Jabaquara
+                coords: {lat:-23.653258, lng: -46.649455},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Jabaquara</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Rua Jupatis, 140 - Vila Mira</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Moóca
+                coords: {lat:-23.547460, lng: -46.602705},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Moóca</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R, Av. Pires do Rio, 600 - Belém</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Olinda
+                coords: {lat:-23.634093, lng: -46.758048},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Olinda</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Nelsom Brissac, 1235 - Parque Regina</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Olinda
+                coords: {lat:-23.634093, lng: -46.758048},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Olinda</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>R. Nelsom Brissac, 1235 - Parque Regina</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+
+            {	//Ecoponto Vila Guilherme
+                coords: {lat:-23.517127, lng: -46.600380},
+                iconImage: 'https://img.icons8.com/bubbles/50/000000/trash.png',
+                content: '<h2 style="text-align: center;">Ecoponto Olinda</h2> <img class="icone" src="../static/images/iconepopup.png" alt=""> <br> <h3>Endereço:</h3> <p>Rua José Bernardo Pinto, 1480 - Vila Guilherme</p> <br> <h3>Horário:</h3> <p>Abre de seg à domingo das 6:00 às 22hs. Aos domingos fecha às 18hs.</p>'
+            },
+        ]
         //loop pelos marcadores
     for(let i = 0; i < markers.length; i++) {
         addMarker(markers[i])
@@ -340,7 +486,7 @@ function initMap() {
                         (function(place) {
                             let markerSearch = new google.maps.Marker({
                                 position: place.geometry.location,
-                                icon: '../static/images/teste-mark.png',
+                                icon: '../static/images/marcadorpesquisa.png',
                             })
                         
                     markerSearch.bindTo('map', searchBox, 'map')
