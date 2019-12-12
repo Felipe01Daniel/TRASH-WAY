cd75c6a1bf1ef214a00485e9762e251d9bc6522f
mouzinho08 <thiagort.tm@gmail.com>
2019-12-12 18:26:06 -0300

    Arrumar pro heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn config_trashway.wsgi
--- config_trashway/settings.py
+++ config_trashway/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'y0ye7z&-ygn+oz(2)$^$i8v9o8t)bs24h2(p4*14xrl7yc6sjp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'trashway.herokuapp.com']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
